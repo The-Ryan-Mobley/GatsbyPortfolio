3a0494c68597dbd2b6bb1c951ce25fe7aaf4439c
Ryan Mobley <ryan.mobley94@gmail.com>
2020-01-28 08:02:17 -0600

    changes to metadata

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Ryan Mobley`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `My portfolio as a JavaScriptDeveloper`,
+    author: `Ryan Mobley`,
   },
   proxy: {
     prefix: "/api",
