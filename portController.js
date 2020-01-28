ace83d30045c73353eff3340a8de4cb09caff05a
Ryan Mobley <ryan.mobley94@gmail.com>
2020-01-27 21:57:33 -0600

    removed more console logs

--- portController.js
+++ portController.js
@@ -1,16 +1,13 @@
 const connection = require('./dbConnect');
 module.exports = {
     queryportfolio: (req, res) => {
-        console.log("we hit");
         const sqlString = `SELECT * FROM projects`;
         connection.query(sqlString, (er, response) =>{
             if(er) throw er;
-            console.log(response);
             response.forEach(i => {
                 let techArr = i.technology.split(",");
                 i.technology = techArr;
             });
-            console.log(response);
             res.json(response);
 
         });
