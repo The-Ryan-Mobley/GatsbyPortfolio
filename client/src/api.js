39bb885823b1f34106122e1ca6c170c7bad3e95c
The-Ryan-Mobley <The-Ryan-Mobley@users.noreply.github.com>
2024-04-02 21:30:32 +0000

    deploy: 3e12e6f105ab9727f0351a897f4eac0cae8040b9

--- client/src/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-export default {
-    getPieces: () => {
-        return axios.get("/api/pieces/", {params: "huh"});
-    }
-}
--- routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const controller = require("../../portController");
-
-router.route("/pieces/").get(controller.queryportfolio);
-
-module.exports = router;
