3b2f4095878447d4ce105ac56ee7e4bcea6e6a98
Ryan Mobley <ryan.mobley94@gmail.com>
2024-04-08 16:53:36 -0500

    build

---
+++ component---src-pages-404-js-b3a06968f3548511e3c0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Wbzz"),o=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."),l.a.createElement(u.Link,{to:"/"},"Return to About Me"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b3a06968f3548511e3c0.js.map
